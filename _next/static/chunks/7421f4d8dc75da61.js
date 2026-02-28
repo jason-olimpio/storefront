5d5289c08c308d85eb3a319cd6000b35141d6513
Jason <olimpiojason@gmail.com>
2026-02-28 22:13:06 +0100

    Updates

---
+++ _next/static/chunks/7421f4d8dc75da61.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,52683,e=>{"use strict";var t=e.i(71645);function o(){return(0,t.useEffect)(()=>{window.location.replace("./en")},[]),null}e.s(["default",()=>o])}]);
